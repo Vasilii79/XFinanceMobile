68ab8b46aee888e88d96c722901628e6b36ca545
XFinance Mobile <78757147+XfinanceMobile@users.noreply.github.com>
2021-02-11 00:45:58 +0100

    XFIMSwap.sol

--- XFIMSwap.sol
+++ XFIMSwap.sol
@@ -791,15 +791,15 @@ contract Ownable is Context {
     }
 }
 
-// File: contracts/XSwap.sol
+// File: contracts/XFIMSwap.sol
 
 pragma solidity 0.6.12;
 
 
 
 
-// XSwap with Governance.
-contract XSwap is ERC20("XSwap", "XSP"), Ownable {
+// XFIMSwap with Governance.
+contract XFIMSwap is ERC20("XFIMSwap", "XFIM"), Ownable {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
@@ -908,9 +908,9 @@ contract XSwap is ERC20("XSwap", "XSP"), Ownable {
         );
 
         address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "XSwap::delegateBySig: invalid signature");
-        require(nonce == nonces[signatory]++, "XSwap::delegateBySig: invalid nonce");
-        require(now <= expiry, "XSwap::delegateBySig: signature expired");
+        require(signatory != address(0), "XFIMSwap::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "XFIMSwap::delegateBySig: invalid nonce");
+        require(now <= expiry, "XFIMSwap::delegateBySig: signature expired");
         return _delegate(signatory, delegatee);
     }
 
@@ -940,7 +940,7 @@ contract XSwap is ERC20("XSwap", "XSP"), Ownable {
         view
         returns (uint256)
     {
-        require(blockNumber < block.number, "XSwap::getPriorVotes: not yet determined");
+        require(blockNumber < block.number, "XFIMSwap::getPriorVotes: not yet determined");
 
         uint32 nCheckpoints = numCheckpoints[account];
         if (nCheckpoints == 0) {
@@ -977,7 +977,7 @@ contract XSwap is ERC20("XSwap", "XSP"), Ownable {
         internal
     {
         address currentDelegate = _delegates[delegator];
-        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying XSwaps (not scaled);
+        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying XFIMSwaps (not scaled);
         _delegates[delegator] = delegatee;
 
         emit DelegateChanged(delegator, currentDelegate, delegatee);
@@ -1013,7 +1013,7 @@ contract XSwap is ERC20("XSwap", "XSP"), Ownable {
     )
         internal
     {
-        uint32 blockNumber = safe32(block.number, "XSwap::_writeCheckpoint: block number exceeds 32 bits");
+        uint32 blockNumber = safe32(block.number, "XFIMSwap::_writeCheckpoint: block number exceeds 32 bits");
 
         if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
             checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
